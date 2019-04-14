8fb9ccc2eda79f7c47cb740dd5c81cdd4ff18d5e
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-04-14 18:40:30 +0200

    buzz: refactor sampling logic

--- buzz/src/main.rs
+++ buzz/src/main.rs
@@ -3,22 +3,12 @@ use geo::vec3::Vec3;
 use buzz::camera::Camera;
 use buzz::material::Material;
 use buzz::sphere::Sphere;
-use buzz::Scene;
+use buzz::{render, RenderConfig, Scene};
 
 fn main() {
-    // try to avoid aliasing by shooting multiple slightly different rays per
-    // pixel and average the colors.
-    let num_samples = 10;
-    // let num_samples = 100;
-
     let target = Vec3::new(0.0, 0.0, -1.0);
-    let camera = Camera::look_at(
-        Vec3::new(3.0, 3.0, 2.0),
-        target,
-        Vec3::new(0.0, 1.0, 0.0),
-        20.0,
-    )
-    .with_focus(target, 2.0);
+    let camera = Camera::look_at(Vec3::zero(), target, Vec3::new(0.0, 1.0, 0.0), 90.0)
+        .with_focus(target, 0.25);
 
     let scene = Scene::new(vec![
         Sphere::new(
@@ -43,14 +33,6 @@ fn main() {
                 fuzziness: 0.3,
             },
         ),
-        // Sphere::new(
-        //     Vec3::new(-1.0, 0.0, -1.0),
-        //     0.5,
-        //     Material::Metal {
-        //         albedo: Vec3::new(0.8, 0.8, 0.8),
-        //         fuzziness: 0.8,
-        //     },
-        // ),
         Sphere::new(
             Vec3::new(-1.0, 0.0, -1.0),
             0.5,
@@ -62,6 +44,16 @@ fn main() {
 
     let mut rng = rand::thread_rng();
 
-    let img = buzz::render(&camera, &scene, (400, 200), num_samples, &mut rng);
+    let img = render(
+        &camera,
+        &scene,
+        &mut rng,
+        &RenderConfig {
+            width: 400,
+            height: 200,
+            samples: 10,
+            max_bounces: 50,
+        },
+    );
     img.save("img.ppm").unwrap();
 }
