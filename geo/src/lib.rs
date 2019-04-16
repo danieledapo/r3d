5e63d90a69eabbd384dd4447278d78056a00032c
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-04-16 21:16:25 +0200

    geo: add axis enum and make vec3 indexable by axis

--- geo/src/lib.rs
+++ geo/src/lib.rs
@@ -3,3 +3,11 @@ pub mod sphere;
 pub mod stl;
 pub mod triangle;
 pub mod vec3;
+
+/// An enum over the X, Y and Z axis.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Axis {
+    X,
+    Y,
+    Z,
+}
