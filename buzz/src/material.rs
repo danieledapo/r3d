795ebffd850fa9e634db2097c1506fe5eec75040
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-04-27 23:14:56 +0200

    buzz/material: add and use constructor functions to hide enum details
    This will be handy with the upcoming material refactoring.

--- buzz/src/material.rs
+++ buzz/src/material.rs
@@ -8,27 +8,42 @@ use geo::Vec3;
 /// an `albedo` field which is the intrinsic color of the material.
 #[derive(Debug, PartialEq, Clone)]
 pub enum Material {
+    Lambertian { albedo: Vec3 },
+    Metal { albedo: Vec3, fuzziness: f64 },
+    Dielectric { refraction_index: f64 },
+    Light { emittance: Vec3 },
+}
+
+impl Material {
     /// The `Lambertian` material is a perfectly matte or diffuse surface which
     /// is modeled after the [Lambertian reflectance model][0].
     ///
     /// [0]: https://en.wikipedia.org/wiki/Lambertian_reflectance
-    Lambertian { albedo: Vec3 },
+    pub const fn lambertian(albedo: Vec3) -> Self {
+        Material::Lambertian { albedo }
+    }
 
     /// A metallic material that reflects light as it comes in. The `fuzziness`
     /// attribute is how much to perturbate each reflected ray. A low value of
     /// `fuzziness` makes it reflect more accurately because the reflected rays
     /// will change less. On the other hand, an high value will make it a bit
     /// opaque while still reflecting its surroundings.
-    Metal { albedo: Vec3, fuzziness: f64 },
+    pub const fn metal(albedo: Vec3, fuzziness: f64) -> Self {
+        Material::Metal { albedo, fuzziness }
+    }
 
     /// Clear materials like glass and diamond are of type Dielectric and are
     /// identified by a refracion index. For example, glass has a refraction
     /// index in [1.3, 1.7] while diamond is 2.4.
-    Dielectric { refraction_index: f64 },
+    pub const fn dielectric(refraction_index: f64) -> Self {
+        Material::Dielectric { refraction_index }
+    }
 
     /// A light material is a material that does not reflect rays, but always
     /// emits the given light.
-    Light { emittance: Vec3 },
+    pub const fn light(emittance: Vec3) -> Self {
+        Material::Light { emittance }
+    }
 }
 
 pub fn lambertian_bounce(intersection: Vec3, n: Vec3, rng: &mut impl Rng) -> Ray {
