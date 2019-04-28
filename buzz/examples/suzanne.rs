e8318fde2c5592be66f04a63e97f5fd04a4e3b2c
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-04-28 19:11:09 +0200

    buzz/facet: add option for flat/smooth shading
    Also, tune the suzanne example.

--- buzz/examples/suzanne.rs
+++ buzz/examples/suzanne.rs
@@ -8,50 +8,54 @@ use buzz::camera::Camera;
 use buzz::facet::Facet;
 use buzz::material::Material;
 use buzz::sphere::Sphere;
-use buzz::Object;
-use buzz::{parallel_render, Environment, RenderConfig, Scene};
+use buzz::{parallel_render, Environment, Object, RenderConfig, Scene};
 
 const MESH_MATERIAL: Material = Material::lambertian(Vec3::new(0.8, 0.1, 0.1));
+// const MESH_MATERIAL: Material = Material::dielectric(2.4);
 
 pub fn main() -> io::Result<()> {
     let camera = Camera::look_at(
-        Vec3::new(2.7, -2.7, 2.0),
+        Vec3::new(0.0, -4.0, 0.0),
         Vec3::new(0.0, 0.0, 0.0),
         Vec3::new(0.0, 0.0, 1.0),
         35.0,
     );
-
-    let mut scene: Vec<Box<dyn Object>> = vec![
-        Box::new(Sphere::new(
-            Vec3::new(-1.5, -4.5, 0.3),
-            1.0,
-            Material::light(Vec3::new(9.0, 9.0, 9.0)),
-        )),
-        Box::new(Sphere::new(
-            Vec3::new(2.1, 4.5, 0.3),
-            1.0,
-            Material::light(Vec3::new(9.0, 9.0, 9.0)),
-        )),
-    ];
+    // let camera = Camera::look_at(
+    //     Vec3::new(0.0, 0.0, 2.0),
+    //     Vec3::new(0.0, 0.0, 0.0),
+    //     Vec3::new(0.0, 1.0, 0.0),
+    //     80.0,
+    // );
 
     let (_, tris) = stl::load_binary_stl(BufReader::new(Cursor::new(
         &include_bytes!("../../data/suzanne.stl")[..],
     )))?;
 
-    for t in tris {
-        let t = t?;
-        scene.push(Box::new(Facet::new(t, &MESH_MATERIAL)));
-    }
+    let mut scene: Vec<Box<dyn Object>> = tris
+        .into_iter()
+        .map(|t| {
+            let t = t?;
+            Ok(Box::new(Facet::new(t, &MESH_MATERIAL, true)) as Box<dyn Object>)
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+
+    scene.push(Box::new(Sphere::new(
+        Vec3::new(0.0, -10000.0, 0.0),
+        9000.0,
+        Material::light(Vec3::new(1.0, 1.0, 1.0)),
+    )));
 
     let environment = Environment::Color(Vec3::new(0.2, 0.3, 0.36));
+    // let environment = Environment::Color(Vec3::new(0.8, 0.9, 0.8));
+
     let img = parallel_render(
         &camera,
         &Scene::new(scene, environment),
         &RenderConfig {
-            width: 1920 / 2,
-            height: 1080 / 2,
+            width: 1920,
+            height: 1080,
             max_bounces: 50,
-            samples: 50,
+            samples: 100,
         },
     );
 
--- buzz/src/facet.rs
+++ buzz/src/facet.rs
@@ -12,16 +12,18 @@ pub struct Facet<'a> {
     positions: [Vec3; 3],
     normal: Vec3,
     material: &'a Material,
+    flat_shading: bool,
 }
 
 impl<'a> Facet<'a> {
-    pub fn new(tri: stl::StlTriangle, material: &'a Material) -> Self {
+    pub fn new(tri: stl::StlTriangle, material: &'a Material, flat_shading: bool) -> Self {
         let normal = geo::triangle::normal(&tri.positions[0], &tri.positions[1], &tri.positions[2]);
 
         Facet {
             positions: tri.positions,
             normal,
             material,
+            flat_shading,
         }
     }
 }
@@ -47,19 +49,21 @@ impl<'a> Object for Facet<'a> {
     }
 
     fn normal_at(&self, pt: Vec3) -> Vec3 {
-        // triangle::normal(&self.positions[0], &self.positions[1], &self.positions[2])
+        if self.flat_shading {
+            self.normal
+        } else {
+            let bary = triangle::barycentric(
+                (&self.positions[0], &self.positions[1], &self.positions[2]),
+                &pt,
+            )
+            .expect("point outside triangle");
 
-        let bary = triangle::barycentric(
-            (&self.positions[0], &self.positions[1], &self.positions[2]),
-            &pt,
-        )
-        .expect("point outside triangle");
+            let mut n = self.positions[0] * bary.x;
+            n += self.positions[1] * bary.y;
+            n += self.positions[2] * bary.z;
+            n.normalize();
 
-        let mut n = self.positions[0] * bary.x;
-        n += self.positions[1] * bary.y;
-        n += self.positions[2] * bary.z;
-        n.normalize();
-
-        n
+            n
+        }
     }
 }
