ecc1cc0e9f48afe4e968638be1f5656fb2e60fc8
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-05-11 22:26:14 +0200

    buzz: split objects in geometry and objects

--- buzz/src/sphere.rs
+++ buzz/src/sphere.rs
@@ -8,38 +8,54 @@ use crate::{Hit, Object, Surface};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Sphere {
+    pub geom: SphereGeometry,
+    pub material: Material,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct SphereGeometry {
     pub center: Vec3,
     pub radius: f64,
-    pub material: Material,
 }
 
 impl Sphere {
     pub fn new(center: Vec3, radius: f64, material: Material) -> Self {
         Sphere {
-            center,
-            radius,
+            geom: SphereGeometry { center, radius },
             material,
         }
     }
 }
 
-impl<'a> Object<'a> for Sphere {
+impl Object<'_> for Sphere {
     fn material(&self) -> &Material {
         &self.material
     }
 
     fn bounding_sphere(&self) -> (Vec3, f64) {
-        (self.center, self.radius)
+        (self.geom.center, self.geom.radius)
+    }
+}
+
+impl<'s> Shape<'s> for Sphere {
+    type Intersection = Hit<'s>;
+
+    fn bbox(&self) -> Aabb {
+        self.geom.bbox()
+    }
+
+    fn intersection(&'s self, ray: &Ray) -> Option<Self::Intersection> {
+        self.geom.intersection(ray)
     }
 }
 
 impl Surface for Sphere {
     fn normal_at(&self, pt: Vec3) -> Vec3 {
-        sphere::normal(self.center, pt)
+        self.geom.normal_at(pt)
     }
 }
 
-impl<'s> Shape<'s> for Sphere {
+impl<'s> Shape<'s> for SphereGeometry {
     type Intersection = Hit<'s>;
 
     fn bbox(&self) -> Aabb {
@@ -50,5 +66,11 @@ impl<'s> Shape<'s> for Sphere {
         let t = sphere::ray_intersection(self.center, self.radius, ray)?;
 
         Some(Hit { t, surface: self })
+    }
+}
+
+impl Surface for SphereGeometry {
+    fn normal_at(&self, pt: Vec3) -> Vec3 {
+        sphere::normal(self.center, pt)
     }
 }
