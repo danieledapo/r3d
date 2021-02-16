08e4e2cb52be7b2fce45cd79eb03aa2b22e286e5
Daniele D'Orazio <d.dorazio96@gmail.com>
2021-02-16 15:22:37 +0100

    l/object: add grid object to render graphs and heightmaps-like data

---
+++ l/examples/fun.rs
@@ -0,0 +1,28 @@
+use std::sync::Arc;
+
+use geo::{util::opener, Vec3};
+
+use l::*;
+
+pub fn main() -> opener::Result<()> {
+    let fun = Grid::from_fn((-3.0, -3.0), (3.0, 3.0), 100, |x, y| {
+        let x2 = x * x;
+        let y2 = y * y;
+
+        2.0 * (x2 + y2).sin() / (x2 + y2 + 0.0001)
+    });
+
+    let scene = Scene::new(vec![Arc::new(fun) as Arc<dyn Object>]);
+
+    let camera = Camera::look_at(
+        Vec3::new(8.0, 8.0, 10.0),
+        Vec3::zero(),
+        Vec3::new(0.0, 0.0, -1.0),
+    )
+    .with_perspective_projection(30.0, 1.0, 0.01, 100.0);
+
+    let paths = render(camera, &scene, &Settings { eps: 0.01 });
+    dump_svg("fun.svg", &paths, SvgSettings::new(1024.0, 1024.0)).expect("cannot save fun.svg");
+
+    opener::open("fun.svg")
+}
---
+++ l/src/object/grid.rs
@@ -0,0 +1,93 @@
+use geo::{
+    ray::Ray,
+    spatial_index::{Bvh, Intersection, Shape},
+    Aabb, Vec3,
+};
+
+use crate::{Facet, Object, Polyline};
+
+#[derive(Debug)]
+pub struct Grid {
+    bvh: Bvh<Facet>,
+    heightmap: Vec<Vec3>,
+    steps: usize,
+    bbox: Aabb,
+}
+
+impl Grid {
+    pub fn from_fn(
+        (sx, sy): (f64, f64),
+        (ex, ey): (f64, f64),
+        steps: u16,
+        fun: impl Fn(f64, f64) -> f64,
+    ) -> Self {
+        assert!(steps >= 2);
+
+        let steps = usize::from(steps);
+        let mut heightmap = Vec::with_capacity(steps.pow(2));
+
+        for i in 0..steps {
+            let y = sy + (ey - sy) * (i as f64) / (steps as f64 - 1.0);
+            for j in 0..steps {
+                let x = sx + (ex - sx) * (j as f64) / (steps as f64 - 1.0);
+
+                heightmap.push(Vec3::new(x, y, fun(x, y)));
+            }
+        }
+
+        let mut bbox = Aabb::new(heightmap[0]);
+        let mut facets = Vec::with_capacity(steps.pow(2));
+
+        for y in 0..steps - 1 {
+            for x in 0..steps - 1 {
+                let a = heightmap[y * steps + x];
+                let b = heightmap[y * steps + x + 1];
+                let c = heightmap[(y + 1) * steps + x];
+                let d = heightmap[(y + 1) * steps + x + 1];
+
+                bbox.extend(&[a, b, c, d]);
+                facets.push(Facet::new([a, c, b]));
+                facets.push(Facet::new([b, c, d]));
+            }
+        }
+
+        Self {
+            heightmap,
+            steps,
+            bbox,
+            bvh: facets.into_iter().collect(),
+        }
+    }
+}
+
+impl Shape for Grid {
+    type Intersection = f64;
+
+    fn intersection(&self, ray: &Ray) -> Option<Self::Intersection> {
+        self.bvh
+            .intersections(ray)
+            .map(|(_, t)| t)
+            .min_by(|t0, t1| t0.t().partial_cmp(&t1.t()).unwrap())
+    }
+
+    fn bbox(&self) -> Aabb {
+        self.bbox.clone()
+    }
+}
+
+impl Object for Grid {
+    fn paths(&self) -> Vec<Polyline> {
+        let mut out = Vec::with_capacity(2 * self.steps);
+
+        for i in 0..self.steps {
+            out.push(self.heightmap[i * self.steps..(i + 1) * self.steps].to_vec());
+            out.push(
+                (0..self.steps)
+                    .map(|j| self.heightmap[j * self.steps + i])
+                    .collect(),
+            );
+        }
+
+        out
+    }
+}
