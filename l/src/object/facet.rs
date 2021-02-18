6965150595a46131e7040e031a934b6941420582
Daniele D'Orazio <d.dorazio96@gmail.com>
2021-02-18 14:08:46 +0100

    l/facet: simple hatching

--- l/src/object/facet.rs
+++ l/src/object/facet.rs
@@ -5,11 +5,20 @@ use crate::Object;
 #[derive(Debug)]
 pub struct Facet {
     triangle: Triangle,
+    hatching_lines: u16,
 }
 
 impl Facet {
     pub fn new(triangle: Triangle) -> Self {
-        Self { triangle }
+        Self {
+            triangle,
+            hatching_lines: 0,
+        }
+    }
+
+    pub fn with_hatching_lines(mut self, lines: u16) -> Self {
+        self.hatching_lines = lines;
+        self
     }
 }
 
@@ -27,12 +36,35 @@ impl Shape for Facet {
 
 impl Object for Facet {
     fn paths(&self) -> Vec<Polyline> {
-        vec![vec![
+        let mut paths = vec![vec![
             self.triangle.a,
             self.triangle.b,
             self.triangle.c,
             self.triangle.a,
         ]
-        .into()]
+        .into()];
+
+        if self.hatching_lines > 0 {
+            let Triangle { a, b, c } = self.triangle;
+
+            let &(s, e1, e2) = [(a, b, c), (b, c, a), (c, a, b)]
+                .iter()
+                .max_by(|(a1, b1, c1), (a2, b2, c2)| {
+                    let d1 = f64::from(a1.dist2(b1) - a1.dist2(c1));
+                    let d2 = f64::from(a2.dist2(b2) - a2.dist2(c2));
+                    d1.partial_cmp(&d2).unwrap()
+                })
+                .unwrap();
+
+            let d1 = e1 - s;
+            let d2 = e2 - s;
+
+            paths.extend((0..self.hatching_lines).map(|i| {
+                let t = f64::from(i + 1) / f64::from(self.hatching_lines + 1);
+                Polyline::from(vec![s + d1 * t, s + d2 * t])
+            }));
+        }
+
+        paths
     }
 }
