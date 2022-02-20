740f852e1a1b653b7aedbf11ee3f132e33037aac
Daniele D'Orazio <d.dorazio96@gmail.com>
2022-02-20 21:43:13 +0100

    geo/util: add iterator over a range of f64

--- geo/src/util/mod.rs
+++ geo/src/util/mod.rs
@@ -1,1 +1,31 @@
 pub mod opener;
+
+pub fn arange(s: f64, e: f64, step: f64) -> impl Iterator<Item = f64> {
+    Arange { s, e, step }
+}
+
+#[derive(Debug, Clone)]
+struct Arange {
+    s: f64,
+    e: f64,
+    step: f64,
+}
+
+impl Iterator for Arange {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.s > self.e {
+            return None;
+        }
+
+        let v = self.s;
+        self.s += self.step;
+        Some(v)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let n = f64::ceil((self.e - self.s) / self.step) as u64;
+        (0, usize::try_from(n).ok())
+    }
+}
