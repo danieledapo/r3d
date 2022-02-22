98863f59189f751a29625ffe284fa9542fc0f1b6
Daniele D'Orazio <d.dorazio96@gmail.com>
2022-02-22 22:38:32 +0100

    geo/sdf: more sdfs

--- geo/src/sdf/primitives.rs
+++ geo/src/sdf/primitives.rs
@@ -18,6 +18,24 @@ pub struct Cylinder {
     height: f64,
 }
 
+#[derive(Debug, Clone)]
+pub struct Torus {
+    r1: f64,
+    r2: f64,
+}
+
+#[derive(Debug, Clone)]
+pub struct Capsule {
+    a: Vec3,
+    b: Vec3,
+    r: f64,
+}
+
+#[derive(Debug, Clone)]
+pub struct Octahedron {
+    r: f64,
+}
+
 impl Sphere {
     pub fn new(radius: f64) -> Self {
         Sphere { radius }
@@ -33,6 +51,24 @@ impl Cuboid {
 impl Cylinder {
     pub fn new(radius: f64, height: f64) -> Self {
         Cylinder { radius, height }
+    }
+}
+
+impl Torus {
+    pub fn new(r1: f64, r2: f64) -> Self {
+        Self { r1, r2 }
+    }
+}
+
+impl Capsule {
+    pub fn new(a: Vec3, b: Vec3, r: f64) -> Self {
+        Self { a, b, r }
+    }
+}
+
+impl Octahedron {
+    pub fn new(r: f64) -> Self {
+        Self { r }
     }
 }
 
@@ -98,5 +134,46 @@ impl Sdf for Cylinder {
         let b = (x.powi(2) + y.powi(2)).sqrt();
 
         a + b
+    }
+}
+
+impl Sdf for Torus {
+    fn dist(&self, p: &Vec3) -> f64 {
+        let q = Vec3::new(Vec3::new(p.x, p.y, 0.0).norm() - self.r2, p.z, 0.0);
+        q.norm2() - self.r1
+    }
+
+    fn bbox(&self) -> Aabb {
+        let a = self.r1;
+        let b = self.r1 + self.r2;
+
+        Aabb::new(Vec3::new(-b, -b, -a)).expanded(Vec3::new(b, b, a))
+    }
+}
+
+impl Sdf for Capsule {
+    fn dist(&self, p: &Vec3) -> f64 {
+        let pa = *p - self.a;
+        let ba = self.b - self.a;
+        let h = f64::clamp(pa.dot(ba) / ba.dot(ba), 0.0, 1.0);
+        (pa - ba * h).norm() - self.r
+    }
+
+    fn bbox(&self) -> Aabb {
+        Aabb::new(self.a - self.r)
+            .expanded(self.a + self.r)
+            .expanded(self.b - self.r)
+            .expanded(self.b + self.r)
+    }
+}
+
+impl Sdf for Octahedron {
+    fn dist(&self, p: &Vec3) -> f64 {
+        let m = p.x.abs() + p.y.abs() + p.z.abs() - self.r;
+        m * f64::to_radians(30.0).tan()
+    }
+
+    fn bbox(&self) -> Aabb {
+        Aabb::new(Vec3::replicate(-self.r)).expanded(Vec3::replicate(self.r))
     }
 }
