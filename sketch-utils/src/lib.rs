b322c4bef0e1b1677c6992c21cd93d5dcf9e3682
Daniele D'Orazio <d.dorazio96@gmail.com>
2022-06-29 17:03:27 +0200

    sketch-utils: add utility to generate a unique name for an iteration of a sketch

--- sketch-utils/src/lib.rs
+++ sketch-utils/src/lib.rs
@@ -1,3 +1,9 @@
+use std::{
+    fs, io,
+    path::Path,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
 pub mod opener;
 
 #[macro_export]
@@ -8,4 +14,28 @@ macro_rules! chrono {
         println!("{} took {}ms", $label, now.elapsed().as_millis());
         res
     }};
+}
+
+pub fn sketch_output_path(sketch: &str) -> io::Result<String> {
+    let sketch = Path::new(sketch);
+    let name = sketch.file_stem().unwrap();
+    let ext = sketch.extension().unwrap();
+
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("..").join(name);
+
+    if !dir.exists() {
+        fs::create_dir(&dir)?;
+    }
+
+    let name = format!(
+        "{}-{}.{}",
+        name.to_str().unwrap(),
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_nanos(),
+        ext.to_str().unwrap()
+    );
+
+    Ok(dir.join(name).to_str().unwrap().to_owned())
 }
