b1069c50f91f89fd26331d7db56b4871c96f37a7
Daniele D'Orazio <daniele@develer.com>
2022-07-01 23:18:57 +0200

    ivo: fix background dimensions
    Previously it was incorrect because it didn't take into account the fact
    that the scaling introduced by the width/height parameters in svg keep
    the aspect ratio and thus there's no background in the parts where
    there's no image (think of the padding used to center the image to keep
    the aspect ratio).

--- ivo/src/renderer/svg.rs
+++ ivo/src/renderer/svg.rs
@@ -23,18 +23,18 @@ pub fn dump_outlines_svg(path: &str, lines: &[Line], settings: &SvgSettings) -> 
         path,
         settings,
         || lines.iter().flat_map(|l| l.iter().copied()),
-        |f, stroke_width| {
+        |f, origin, sf| {
             // all the lines share the same attributes hence using a group allows to
             // save a lot of space in the final SVG given that such attributes are not
             // repeated.
             writeln!(
                 f,
                 r#"<g stroke="{}" stroke-width="{}" fill="none">"#,
-                settings.stroke, stroke_width
+                settings.stroke, settings.stroke_width,
             )?;
 
             for l in lines {
-                dump_polyline(f, l, settings.digits)?;
+                dump_polyline(f, origin, sf, l, settings.digits)?;
             }
 
             writeln!(f, "</g>")?;
@@ -53,14 +53,14 @@ pub fn dump_triangles_svg(
         path,
         settings,
         || triangles.iter().flat_map(|l| l.pts.iter().copied()),
-        |f, stroke_width| {
+        |f, origin, sf| {
             for orient in [Orientation::Top, Orientation::Left, Orientation::Right] {
                 let fill = settings.fill_colors[orient as usize];
 
                 writeln!(
                     f,
                     r#"<g stroke="{fill}" fill="{fill}" stroke-width="{}" >"#,
-                    stroke_width,
+                    settings.stroke_width,
                     fill = fill.unwrap_or("none"),
                 )?;
 
@@ -72,6 +72,8 @@ pub fn dump_triangles_svg(
                     // be sure to close the polyline otherwise glitches occur
                     dump_polyline(
                         f,
+                        origin,
+                        sf,
                         &[t.pts[0], t.pts[1], t.pts[2], t.pts[0]],
                         settings.digits,
                     )?;
@@ -89,7 +91,7 @@ fn svg_prelude<Pts>(
     path: &str,
     settings: &SvgSettings,
     get_pts: impl Fn() -> Pts,
-    render: impl Fn(&mut BufWriter<File>, f64) -> io::Result<()>,
+    render: impl Fn(&mut BufWriter<File>, XY, f64) -> io::Result<()>,
 ) -> io::Result<()>
 where
     Pts: IntoIterator<Item = XY>,
@@ -125,34 +127,52 @@ where
 
     let (width, height) = (maxx - minx, maxy - miny);
 
+    let sf = f64::min(settings.width / width, settings.height / height);
+
     writeln!(
         f,
         r#"<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" width="{}" height="{}" viewBox="{minx} {miny} {width} {height}">"#,
-        settings.width, settings.height
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="{} {} {} {}">"#,
+        -settings.width / 2.0,
+        -settings.height / 2.0,
+        settings.width,
+        settings.height,
     )?;
 
     if let Some(background) = settings.background {
         writeln!(
             f,
-            r#"<rect x="{minx}" y="{miny}" width="{width}" height="{height}" stroke="none" fill="{background}"/>"#,
+            r#"<rect x="{}" y="{}" width="{}" height="{}" stroke="none" fill="{background}"/>"#,
+            -settings.width / 2.0,
+            -settings.height / 2.0,
+            settings.width,
+            settings.height,
         )?;
     }
 
-    // adjust the requested stroke_width according to the scaling factor
-    let sw = settings.stroke_width / f64::max(settings.width / width, settings.height / height);
-
-    render(&mut f, sw)?;
+    render(&mut f, ((minx + maxx) / 2.0, (miny + maxy) / 2.0), sf)?;
 
     writeln!(f, "</svg>")?;
 
     Ok(())
 }
 
-fn dump_polyline(f: &mut impl Write, pts: &[XY], digits: usize) -> io::Result<()> {
+fn dump_polyline(
+    f: &mut impl Write,
+    origin: XY,
+    sf: f64,
+    pts: &[XY],
+    digits: usize,
+) -> io::Result<()> {
     write!(f, r#"<polyline points=""#)?;
     for (x, y) in pts {
-        write!(f, "{x:.digits$},{y:.digits$} ", digits = digits)?;
+        write!(
+            f,
+            "{:.digits$},{:.digits$} ",
+            (x - origin.0) * sf,
+            (y - origin.1) * sf,
+            digits = digits
+        )?;
     }
     writeln!(f, r#"" />"#)?;
     Ok(())
