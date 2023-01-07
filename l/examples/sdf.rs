f9c277f1095c01732791fd6e116b406b819effe9
Daniele D'Orazio <daniele@develer.com>
2023-01-07 18:38:38 +0100

    ray march each layer, it's way faster than before

--- l/examples/sdf.rs
+++ l/examples/sdf.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
-use geo::mat4::Mat4;
-use geo::ray::Ray;
-use geo::Axis;
-use geo::{primitive::polyline::Polyline, sdf::*, spatial_index::Shape, v3, Aabb, Vec3};
+use geo::{
+    mat4::Mat4, primitive::polyline::Polyline, ray::Ray, sdf::*, spatial_index::Shape, v3, Aabb,
+    Axis, Vec3,
+};
+
+use marching_squares::Field;
 use sketch_utils::opener;
 
 use rayon::prelude::*;
@@ -22,9 +24,7 @@ impl Shape for Sp {
     type Intersection = f64;
 
     fn intersection(&self, ray: &geo::ray::Ray) -> Option<Self::Intersection> {
-        let t = self.sdf.ray_march(ray, 128)?;
-        assert!(t.is_finite());
-        Some(t)
+        self.sdf.ray_march(ray, 128)
     }
 
     fn bbox(&self) -> Aabb {
@@ -42,7 +42,10 @@ impl Object for Sp {
                 let mut paths = vec![];
                 let y = bbox.min()[self.axis]
                     + bbox.dimensions()[self.axis] * f64::from(t) / f64::from(self.divs - 1);
+
                 let f = SdfField::new(&self.sdf, y, self.axis);
+
+                // f.debug_save();
 
                 let contours = marching_squares::march(&f, 0.0);
                 for c in contours {
@@ -62,7 +65,7 @@ impl Object for Sp {
                         let n = self.sdf.normal_at(p);
                         let lt = n.dot(-self.light_dir);
 
-                        let black = lt <= ((t % 4) as f64 / 3.0);
+                        let black = lt <= 0.5 + 0.5 * ((t % 4) as f64 / 3.0);
                         if black {
                             cur.push(p);
                         } else if !cur.is_empty() {
@@ -82,28 +85,29 @@ impl Object for Sp {
     }
 }
 
-struct SdfField<'a> {
-    data: &'a Sdf,
+struct SdfField {
+    data: Vec<f64>,
     y: f64,
     axis: Axis,
     width: usize,
     height: usize,
+    bbox: Aabb,
 }
 
-impl<'a> marching_squares::Field for SdfField<'a> {
+impl marching_squares::Field for SdfField {
     fn dimensions(&self) -> (usize, usize) {
         (self.width, self.height)
     }
 
     fn z_at(&self, x: usize, y: usize) -> f64 {
-        self.data.dist(&self.to_3d(x as f64, y as f64))
+        self.data[y * self.width + x]
     }
 }
 
-impl<'a> SdfField<'a> {
+impl SdfField {
     const RESOLUTION: f64 = 10.0;
 
-    pub fn new(sdf: &'a Sdf, y: f64, axis: Axis) -> Self {
+    pub fn new(sdf: &Sdf, y: f64, axis: Axis) -> Self {
         let bbox = sdf.bbox();
         let dims = bbox.dimensions();
 
@@ -118,19 +122,59 @@ impl<'a> SdfField<'a> {
         // +1 to include the last pixel, +2 to add a black border
         let (w, h) = (w + 1 + 2, h + 1 + 2);
 
-        Self {
-            data: sdf,
+        let mut res = Self {
+            bbox: sdf.bbox(),
+            data: vec![100.0; w * h],
             y,
             axis,
             width: w,
             height: h,
+        };
+
+        const EPS: f64 = 0.001;
+        for j in 0..h {
+            // for i in 0..w {
+            //     res.data[j * w + i] = sdf.dist(&res.to_3d(i as f64, j as f64));
+            // }
+            // continue;
+
+            let mut i = 0.0;
+            while i < w as f64 {
+                let d = sdf.dist(&res.to_3d(i, j as f64));
+                if d <= EPS {
+                    res.data[j * w + i as usize] = d;
+
+                    if i > 0.0 && res.data[j * w + i as usize - 1] > EPS {
+                        res.data[j * w + i as usize - 1] = sdf.dist(&res.to_3d(i - 1.0, j as f64));
+                    }
+
+                    if j > 0 && res.data[(j - 1) * w + i as usize] > EPS {
+                        res.data[(j - 1) * w + i as usize] =
+                            sdf.dist(&res.to_3d(i, j as f64 - 1.0));
         }
+
+                    i += 1.0;
+                    continue;
     }
 
-    fn to_3d(&self, i: f64, j: f64) -> Vec3 {
-        let mut p = self.data.bbox().min();
-        p -= 1.0;
+                if i > 0.0 && res.data[j * w + i as usize - 1] <= EPS {
+                    res.data[j * w + i as usize] = d;
+                }
 
+                if j > 0 && res.data[(j - 1) * w + i as usize] <= EPS {
+                    res.data[j * w + i as usize] = d;
+                }
+
+                i += (d * Self::RESOLUTION).floor().max(1.0);
+            }
+        }
+
+        res
+    }
+
+    pub fn to_3d(&self, i: f64, j: f64) -> Vec3 {
+        let mut p = self.bbox.min();
+        p -= 1.0 / Self::RESOLUTION;
         p[self.axis] = self.y;
 
         p += match self.axis {
@@ -139,11 +183,38 @@ impl<'a> SdfField<'a> {
             Axis::Z => v3(i, j, 0),
         } / v3(Self::RESOLUTION, Self::RESOLUTION, Self::RESOLUTION);
 
-        // are the points rounded correctly?
-        p += 0.5;
-
         p
     }
+
+    pub fn debug_save(&self) {
+        use std::{
+            fs::File,
+            io::{BufWriter, Write},
+        };
+
+        let mut out = BufWriter::new(
+            File::create(format!(
+                "debug-{:08}.pbm",
+                (1000000.0 + self.y * 100.0).round() as i64
+            ))
+            .unwrap(),
+        );
+
+        let (w, h) = self.dimensions();
+
+        writeln!(out, "P1").unwrap();
+        writeln!(out, "{} {}", w, h).unwrap();
+
+        for y in 0..h {
+            for x in 0..w {
+                write!(out, "{}", if self.z_at(x, y) < 0.001 { 0 } else { 1 }).unwrap();
+    }
+            writeln!(out).unwrap();
+        }
+    }
+}
+
+
 }
 
 pub fn main() -> opener::Result<()> {
